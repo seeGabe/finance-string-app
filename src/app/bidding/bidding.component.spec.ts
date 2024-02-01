270022741a10e51b4da5b4be11307526bac5dbb8
Gabe Colon <seeGabe@users.noreply.github.com>
2024-01-31 23:00:30 -0500

    tests(bid): add specs

--- src/app/bidding/bidding.component.spec.ts
+++ src/app/bidding/bidding.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BiddingComponent } from './bidding.component';
+import { ReactiveFormsModule, Validators } from '@angular/forms';
 
 describe('BiddingComponent', () => {
   let component: BiddingComponent;
@@ -8,10 +9,9 @@ describe('BiddingComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [BiddingComponent]
-    })
-    .compileComponents();
-    
+      imports: [BiddingComponent, ReactiveFormsModule],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(BiddingComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +19,23 @@ describe('BiddingComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a field', () => {
+    const inputElement =
+      fixture.debugElement.nativeElement.querySelector('#bidControl');
+    expect(inputElement).toBeDefined();
+  });
+
+  it('should validate', () => {
+    const inputElement =
+      fixture.debugElement.nativeElement.querySelector('#bidControl');
+    const inputControl = component.bidControl;
+    inputControl.setValue('');
+    expect(inputElement.value).toEqual(inputControl.value);
+    expect(inputControl.errors).not.toBeNull();
+    expect(inputControl.errors?.['required']).toBeTruthy();
+    inputControl.setValue('foo');
+    expect(inputControl.errors?.['pattern']).toBeTruthy();
   });
 });
