05a3f83b7050c1b036530cc4b08970dfff40132a
Gabe Colon <seeGabe@users.noreply.github.com>
2024-01-31 21:49:09 -0500

    update(bid): add control, on submit

--- src/app/bidding/bidding.component.ts
+++ src/app/bidding/bidding.component.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AppService } from '../app.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-bidding',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './bidding.component.html',
 })
-export class BiddingComponent {}
+export class BiddingComponent {
+  appService = inject(AppService);
+  hasError = false;
+  bidControl = new FormControl('', {
+    nonNullable: true,
+    validators: [
+      Validators.required,
+      Validators.pattern(/\d+[bkmt]|[.]+\d+[bkmt]/),
+    ],
+  });
+
+  onSubmit(): void {
+    if (this.bidControl.valid) {
+      this.hasError = false;
+      this.appService.currentBidSignal.set(this.bidControl.value);
+    } else {
+      this.hasError = true;
+    }
+  }
+}
